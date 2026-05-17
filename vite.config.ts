c01d19f8aa6ba30ee381f9795a3e0a5f355d54eb
developingsystems <284054604+developingsystems@users.noreply.github.com>
2026-05-17 22:17:31 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import cesium from 'vite-plugin-cesium';
 export default defineConfig({
   plugins: [cesium()],
   server: {
-    host: 'true',
+    host: true,
     port: 5173,
     strictPort: false,   // fall back to another port if 5173 is taken
     cors: true,
