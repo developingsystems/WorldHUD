e51958f891c4ab0f392ca4023db617cf285cfdd4
developingsystems <284054604+developingsystems@users.noreply.github.com>
2026-05-20 01:06:21 +0300

    Update infobox.ts

--- src/ui/infobox.ts
+++ src/ui/infobox.ts
@@ -155,7 +155,7 @@ export class InfoBox {
     this.container.id = 'custom-infobox';
     this.container.style.cssText = `
       position: absolute;
-      top: 10px;
+      top: 60px;
       right: 10px;
       width: 340px;
       max-height: 80vh;
