b6b5b47fd279474b43466acaa174d05e83b9d4f2
developingsystems <284054604+developingsystems@users.noreply.github.com>
2026-05-20 04:44:18 +0300

    Update infobox.ts

--- src/ui/infobox.ts
+++ src/ui/infobox.ts
@@ -169,7 +169,7 @@ export class InfoBox {
       top: 44px;
       right: 10px;
       width: 480px;
-      max-height: 75vh;
+      max-height: 67vh;
       overflow-y: auto;
       background: rgba(0, 0, 0, 0.80);
       color: white;
